10402c0095b55d1ea1bd220566312707c5a90fac
Giddius <GordonGrossmann@gmx.at>
2023-05-20 00:38:47 +0200

    added `player.py` file for `Player` class added `players_container.py` file for `Players` class
    suffix `_container` was added to prevent typo-bugs with singular player

---
+++ antistasi_server_analytics/data_types/players_container.py
@@ -0,0 +1,33 @@
+"""
+WiP.
+"""
+
+# region [Imports]
+
+
+from typing import (TYPE_CHECKING, TypeVar, TypeGuard, TypeAlias, Final, TypedDict, Generic, Union, Optional, ForwardRef, final, Callable,
+                    no_type_check, no_type_check_decorator, overload, get_type_hints, cast, Protocol, runtime_checkable, NoReturn, NewType, Literal, AnyStr, IO, BinaryIO, TextIO, Any)
+
+
+if TYPE_CHECKING:
+    ...
+
+# endregion [Imports]
+
+# region [Logging]
+
+
+# endregion [Logging]
+
+# region [Constants]
+
+
+# endregion [Constants]
+
+
+# region [Main_Exec]
+
+if __name__ == '__main__':
+    pass
+
+# endregion [Main_Exec]
