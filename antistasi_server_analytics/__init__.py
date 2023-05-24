05f738017943a0a116ad787ec20df87427a9d3d2
Giddius <GordonGrossmann@gmx.at>
2023-05-24 02:18:38 +0200

    added `__app_name__` and `__app_pretty_name__` dynamic dunders to root `__init__.py`
    can only be gathered by the root `__init__.py` not from `__main__.py`
    as the `__package__` variable is None in the executing file.

--- antistasi_server_analytics/__init__.py
+++ antistasi_server_analytics/__init__.py
@@ -1,3 +1,12 @@
 """Antistasi Server Analytics"""
 
+from pathlib import Path
+
 __version__ = '0.0.1'
+
+if __package__ is None:
+    __app_name__ = THIS_FILE_DIR = Path(__file__).parent.resolve().name
+else:
+    __app_name__ = str(__package__)
+
+__app_pretty_name__ = __app_name__.replace("_", " ").title()
